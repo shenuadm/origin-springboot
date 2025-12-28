d81d584a711b4b141f9f4782ad1dca78ab1d846d
一陌千尘 <18780166539@189.cn>
2025-12-28 18:19:26 +0800

    开启SQL审计功能

--- origin-module-common/src/main/java/com/cosmos/origin/common/config/MybatisFlexConfig.java
+++ origin-module-common/src/main/java/com/cosmos/origin/common/config/MybatisFlexConfig.java
@@ -1,10 +1,13 @@
 package com.cosmos.origin.common.config;
 
+import com.mybatisflex.core.audit.AuditManager;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * Mybatis Flex 配置文件
+ * Mybatis Flex 配置
  *
  * @author 一陌千尘
  * @date 2025/11/03
@@ -13,4 +16,17 @@ import org.springframework.context.annotation.Configuration;
 @MapperScan("com.cosmos.origin.common.domain.mapper")
 public class MybatisFlexConfig {
 
+    private static final Logger logger = LoggerFactory
+            .getLogger("mybatis-flex-sql");
+
+    public MybatisFlexConfig() {
+        // 开启审计功能
+        AuditManager.setAuditEnable(true);
+
+        // 设置 SQL 审计收集器
+        AuditManager.setMessageCollector(auditMessage ->
+                logger.info("{},{}ms", auditMessage.getFullSql()
+                        , auditMessage.getElapsedTime())
+        );
+    }
 }
