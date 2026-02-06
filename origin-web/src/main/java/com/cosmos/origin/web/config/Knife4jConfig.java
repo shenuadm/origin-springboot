fe7d5c786f4a37eda26279017bf7c7358fb973e7
一陌千尘 <mchshenu@gmail.com>
2026-02-06 09:56:05 +0800

    代码优化

--- origin-web/src/main/java/com/cosmos/origin/web/config/Knife4jConfig.java
+++ origin-web/src/main/java/com/cosmos/origin/web/config/Knife4jConfig.java
@@ -1,6 +1,7 @@
 package com.cosmos.origin.web.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
@@ -23,6 +24,9 @@ public class Knife4jConfig {
                         .version("1.0")
                         .description("技术底座单体版 API")
                         .termsOfService("https://doc.xiaominfo.com")
+                        .contact(new Contact()
+                                .name("一陌千尘")
+                                .email("mchshenu@gmail.com"))
                         .license(new License().name("Apache 2.0")
                                 .url("https://doc.xiaominfo.com")));
     }
